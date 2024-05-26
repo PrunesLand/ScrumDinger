7dac964fe4451abffb36f3ee7fcced02a4cc608b
“Pranaya <pranaya.anargya2@homecredit.co.id>
2024-05-26 17:57:52 +0700

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Pranaya Anargya (ID) on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
