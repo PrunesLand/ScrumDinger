22daf11d331d7d58359dc9c4f05b6a87fef30d9a
“Pranaya <pranaya.anargya2@homecredit.co.id>
2024-06-22 18:37:59 +0700

    same as last commit

--- ScrumDinger/Views/DetailedView.swift
+++ ScrumDinger/Views/DetailedView.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+    
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+    
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
